7dad8df06e3ea9f5a786bf87e8fe979d58a77241
aiko <monsteeh@gmail.com>
2025-03-22 17:43:32 +0100

    Starting files

---
+++ src/textnode.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class TextNode(text, text_type, )
+
+
+# Normal text
+# **Bold text**
+# _Italic text_
+# `Code text`
+# Links, in this format: [anchor text](url)
+# Images, in this format: ![alt text](url)
